e570aa3fe47d821d0d8b82a0998a9a48de3352d1
anton-dev-ua <box.of.anton@gmail.com>
2014-12-16 20:52:56 +0100

    test for Format operation (plugin sdk depended)

--- src/test/java/pipetableformatter/PipeTableControllerIntegrationTest.java
+++ src/test/java/pipetableformatter/PipeTableControllerIntegrationTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class EndToEndTest {
+public class PipeTableControllerIntegrationTest {
 
     String notFormattedText =
             "" +
